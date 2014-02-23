3d0695ccfd0b25a9889e7b5683ad40cab3a25295
Ruslan <akhundov2@gmail.com>
2014-02-24 00:50:34 +0400

    check for if expr os correct

--- homework3/src/Main.java
+++ homework3/src/Main.java
@@ -3,103 +3,52 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
+    private static Expression expr;
+    private static ArrayList<String> vars;
+    private static PrintWriter out;
+
+    private static boolean checkExpr(int index, Map<String, Boolean> map) {
+        if (index == vars.size()) {
+            boolean fl = expr.evaluate(map);
+            if (!fl) {
+                out.print("Высказывание ложно при ");
+                for (String var : vars) {
+                    out.print(var + "=");
+                    if (map.get(var)){
+                        out.print("И");
+                    } else {
+                        out.print("Л");
+                    }
+                    out.print(", ");
+                }
+            }
+            return fl;
+        }
+        map.put(vars.get(index), true);
+        boolean fl1 = checkExpr(index + 1, map);
+        map.put(vars.get(index), false);
+        boolean fl2 = checkExpr(index + 1, map);
+        return fl1 && fl2;
+    }
+
+    private static boolean checkExpr() {
+        return checkExpr(0, new TreeMap<String, Boolean>());
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader("input.txt"));
-        PrintWriter out = new PrintWriter("output.txt");
-        ArrayList<Expression> expressions = new ArrayList<Expression>();
-        String s = in.readLine();
-        Parser parser;
-        String[] arg = s.split(",");
-        ArrayList<Expression> added = new ArrayList<Expression>();
-        for (int i = 0; i < arg.length - 1; i++) {
-            parser = new Parser(arg[i]);
-            added.add(parser.parse());
-        }
-        String[] todo = arg[arg.length - 1].split("\\|-");
+        out = new PrintWriter("output.txt");
+        String input = in.readLine();
+        Parser parser = new Parser(input);
+        expr = parser.parse();
+        vars = parser.getVariables();
+        boolean isCorrect = checkExpr();
 
-        Expression alpha = (new Parser(todo[0])).parse();
-        Expression beta = (new Parser(todo[1])).parse();
-        ArrayList<Expression> ans = new ArrayList<Expression>();
-        s = in.readLine();
-        while (s != null) {
-            parser = new Parser(s);
-            expressions.add(parser.parse());
-            s = in.readLine();
-        }
-        boolean isgood = true;
-        int i = 0;
-        for (Expression expr : expressions) {
-            i++;
-            isgood = false;
-            if (Axioms.checker(expr) != -1) {
-                ans.add(expr);
-                ans.add(new Implication(expr, new Implication(alpha, expr)));
-                ans.add(new Implication(alpha, expr));
-                isgood = true;
-                continue;
-            }
-            for (Expression g : added) {
-                if (g.equals(expr)) {
-                    ans.add(expr);
-                    ans.add(new Implication(expr, new Implication(alpha, expr)));
-                    ans.add(new Implication(alpha, expr));
-                    isgood = true;
-
-                }
-                if (isgood) break;
-            }
-            if (alpha.equals(expr)) {
-                ans.add(new Implication(alpha, new Implication(alpha, alpha)));
-                ans.add(new Implication(new Implication(alpha, new Implication(alpha, alpha)),
-                        new Implication(new Implication(alpha, new Implication(
-                                new Implication(alpha, alpha), alpha)), new Implication(alpha, alpha))));
-                ans.add(new Implication(new Implication(alpha, new Implication(new Implication(alpha, alpha), alpha)),
-                        new Implication(alpha, alpha)));
-                ans.add(new Implication(alpha, new Implication(new Implication(alpha, alpha), alpha)));
-                ans.add(new Implication(alpha, alpha));
-                isgood = true;
-                continue;
-            }
-
-            for (int j = 0; j < i - 1; j++) {
-                Expression mp = expressions.get(j);
-                if (mp instanceof Implication) {
-                    Implication impl = (Implication) mp;
-                    if (impl.getRight().equals(expr)) {
-                        for (int k = 0; k < i - 1; k++) {
-                            Expression mp2 = expressions.get(k);
-                            if (mp2.equals(impl.getLeft())) {
-                                ans.add(new Implication(new Implication(alpha, mp2), new Implication(new
-                                        Implication(alpha, new Implication(mp2, expr)), new Implication(alpha, expr))));
-                                ans.add(new Implication(new Implication(alpha, new Implication(mp2, expr)),
-                                        new Implication(alpha, expr)));
-                                ans.add(new Implication(alpha, expr));
-                                isgood = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (isgood)
-                        break;
-                }
-            }
-        }
-        if (isgood) {
-            for (Expression expr : ans) {
-                out.println(expr.toString());
-            }
-
-
-
-        } else {
-            for (Expression expr : ans) {
-                out.println(expr.toString());
-            }
-            out.println("Error");
-        }
         out.close();
     }
 
--- homework3/src/Parser.java
+++ homework3/src/Parser.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+
 public class Parser {
     private String expr;
+    private ArrayList<String> variables;
 
     public Parser(String expr) {
         this.expr = expr.replaceAll("->", ">");
+        this.variables = new ArrayList<String>();
+    }
+
+    public ArrayList<String> getVariables() {
+        return variables;
     }
 
     private Expression parse(int begin, int end) {
@@ -55,6 +63,9 @@ public class Parser {
             while(i < end && (Character.isAlphabetic(expr.charAt(i)) || Character.isDigit(expr.charAt(i)))){
                 s += expr.charAt(i);
                 ++i;
+            }
+            if (!variables.contains(s)){
+                variables.add(s);
             }
             return new Variable(s);
         }
