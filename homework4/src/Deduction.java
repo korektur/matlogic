a3742d3ad35a35a3cfef12037c3a45a00a8baf7d
Ruslan <akhundov2@gmail.com>
2014-03-01 00:02:25 +0400

    hw4 added

---
+++ homework4/src/Deduction.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+
+
+public class Deduction {
+    private Expression alpha;
+    private Expression beta;
+    private ArrayList<Expression> added;
+    private ArrayList<Expression> expressions;
+
+    public Deduction(Expression alpha, Expression beta, ArrayList<Expression> g, ArrayList<Expression> expressions) {
+        this.alpha = alpha;
+        this.beta = beta;
+        this.added = g;
+        this.expressions = expressions;
+    }
+
+    public ArrayList<Expression> getProof() {
+        ArrayList<Expression> ans = new ArrayList<>();
+        boolean isGood = true;
+        int i = 0;
+        for (Expression expr : expressions) {
+            i++;
+            isGood = false;
+            if (Axioms.checker(expr) != -1) {
+                ans.add(expr);
+                ans.add(new Implication(expr, new Implication(alpha, expr)));
+                ans.add(new Implication(alpha, expr));
+                isGood = true;
+                continue;
+            }
+            for (Expression g : added) {
+                if (g.equals(expr)) {
+                    ans.add(expr);
+                    ans.add(new Implication(expr, new Implication(alpha, expr)));
+                    ans.add(new Implication(alpha, expr));
+                    isGood = true;
+
+                }
+                if (isGood) break;
+            }
+            if (alpha.equals(expr)) {
+                ans.add(new Implication(alpha, new Implication(alpha, alpha)));
+                ans.add(new Implication(new Implication(alpha, new Implication(alpha, alpha)),
+                        new Implication(new Implication(alpha, new Implication(
+                                new Implication(alpha, alpha), alpha)), new Implication(alpha, alpha))));
+                ans.add(new Implication(new Implication(alpha, new Implication(new Implication(alpha, alpha), alpha)),
+                        new Implication(alpha, alpha)));
+                ans.add(new Implication(alpha, new Implication(new Implication(alpha, alpha), alpha)));
+                ans.add(new Implication(alpha, alpha));
+                isGood = true;
+                continue;
+            }
+
+            for (int j = 0; j < i - 1; j++) {
+                Expression mp = expressions.get(j);
+                if (mp instanceof Implication) {
+                    Implication impl = (Implication) mp;
+                    if (impl.getRight().equals(expr)) {
+                        for (int k = 0; k < i - 1; k++) {
+                            Expression mp2 = expressions.get(k);
+                            if (mp2.equals(impl.getLeft())) {
+                                ans.add(new Implication(new Implication(alpha, mp2), new Implication(new
+                                        Implication(alpha, new Implication(mp2, expr)), new Implication(alpha, expr))));
+                                ans.add(new Implication(new Implication(alpha, new Implication(mp2, expr)),
+                                        new Implication(alpha, expr)));
+                                ans.add(new Implication(alpha, expr));
+                                isGood = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (isGood)
+                        break;
+                }
+            }
+            if (!isGood)
+                break;
+        }
+        if (isGood) {
+            return ans;
+        } else {
+            throw null;
+        }
+    }
+}
---
+++ homework4/src/Parser.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+
+public class Parser {
+    private String expr;
+    private ArrayList<String> variables;
+
+    public Parser(String expr) {
+        this.expr = expr.replaceAll("->", ">");
+        this.variables = new ArrayList<>();
+    }
+
+    public ArrayList<String> getVariables() {
+        return variables;
+    }
+
+    private Expression parse(int begin, int end) {
+
+        int balance = 0;
+        for (int i = begin; i < end; ++i) {
+            if (expr.charAt(i) == '(') {
+                balance++;
+            }
+            if (expr.charAt(i) == ')') {
+                balance--;
+            }
+            if (expr.charAt(i) == '>' && balance == 0) {
+                return new Implication(parse(begin, i), parse(i + 1, end));
+            }
+        }
+
+        balance = 0;
+        for (int i = begin; i < end; ++i) {
+            if (expr.charAt(i) == '(') {
+                balance++;
+            }
+            if (expr.charAt(i) == ')') {
+                balance--;
+            }
+            if (expr.charAt(i) == '|' && balance == 0) {
+                return new Disjunction(parse(begin, i), parse(i + 1, end));
+            }
+        }
+
+        balance = 0;
+        for (int i = begin; i < end; ++i) {
+            if (expr.charAt(i) == '(') {
+                balance++;
+            }
+            if (expr.charAt(i) == ')') {
+                balance--;
+            }
+            if (expr.charAt(i) == '&' && balance == 0) {
+                return new Conjunction(parse(begin, i), parse(i + 1, end));
+            }
+        }
+
+        if (expr.charAt(begin) == '!') {
+            return new Negation(parse(begin + 1, end));
+        }
+        if (Character.isAlphabetic(expr.charAt(begin))){
+            String s = "";
+            int i = begin;
+            while(i < end && (Character.isAlphabetic(expr.charAt(i)) || Character.isDigit(expr.charAt(i)))){
+                s += expr.charAt(i);
+                ++i;
+            }
+            if (!variables.contains(s)){
+                variables.add(s);
+            }
+            return new Variable(s);
+        }
+        if (expr.charAt(begin) == '('){
+            return parse(begin + 1, end - 1);
+        }
+
+        return null;
+    }
+
+    public Expression parse() {
+        return parse(0, expr.length());
+    }
+}
